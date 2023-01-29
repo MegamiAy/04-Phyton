8ccaf805caefe01b3f00594f929ee8ec50358c9d
Megami.ay <megami.ay@gmail.com>
2023-01-29 18:10:19 -0300

    desafio 005

---
+++ excem/004.py
@@ -0,0 +1,8 @@
+var = input('Digite algo: ')
+print('O tipo de dado recebido foi', type(var))
+print('É numérico? ', var.isnumeric())
+print('É alfabético? ', var.isalpha())
+print('É alfanumérico? ', var.isalnum())
+print('É maiúscula? ', var.isupper())
+print('É minúscula? ', var.islower())
+print('É decimal? ', var.isdecimal())
--- exercicios/tst003.py
+++ exercicios/tst003.py
@@ -1,2 +1,2 @@
-n = float(input('Digite um valor: '))
-print(n)
+n = input('Digite um valor: ')
+print(n.isnumeric())
