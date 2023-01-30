fdac0142e3ab63dfc923db95fce1c71304314767
Megami.ay <megami.ay@gmail.com>
2023-01-30 20:43:30 -0300

    18 crr

--- excem/018.py
+++ excem/018.py
@@ -1,6 +1,7 @@
 import math
-num = int(input('Digite um valor inteiro: '))
-cos = math.acos(num)
-sen = math.asin(num)
-tan = math.atan(num)
-print('O ângulo cosseno {}, o seno {} e o tangente{}'.format(cos, sen, tan))
+num = float(input('Digite o ângulo: '))
+ra = math.radians(num)
+cos = math.cos(ra)
+sen = math.sin(ra)
+tan = math.tan(ra)
+print('No ângulo {} \no cosseno é {:.2f} \no seno é {:.2f} \no tangente é {:.2f}'.format(num, cos, sen, tan))
