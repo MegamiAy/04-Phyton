47ca0e1c66fdefd84747d867d3db45f6eac8d34a
Megami.ay <megami.ay@gmail.com>
2023-01-30 20:36:05 -0300

    change

--- excem/017.py
+++ excem/017.py
@@ -2,4 +2,4 @@ from math import hypot
 x = float(input('digite o comprimento do cateto oposto: '))
 y = float(input('digite o comprimento do cateto adjacente: '))
 h = hypot(x, y)
-print('A hipotenusa é {}'.format(h))
+print('A hipotenusa é {:.2f}'.format(h))
