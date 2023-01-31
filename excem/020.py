4b1811578ee39d970cd0e81d3698acf4b37ed11a
Megami.ay <megami.ay@gmail.com>
2023-01-30 21:02:46 -0300

    msc

--- excem/020.py
+++ excem/020.py
@@ -3,3 +3,7 @@ n1 = input('Digite o nome: ')
 n2 = input('Digite o nome: ')
 n3 = input('Digite o nome: ')
 n4 = input('Digite o nome: ')
+list = [n1, n2, n3, n4]
+random.shuffle(list)
+print('A squência de apresentação é:')
+print(list)
