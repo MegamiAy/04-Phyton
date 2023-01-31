b129b18b1f46d65bd72b34f1cda8ac037771884d
Megami.ay <megami.ay@gmail.com>
2023-01-30 21:27:45 -0300

    dsf021

--- excem/021.py
+++ excem/021.py
@@ -1,7 +1,5 @@
-import wave
-aud = wave.open('disagree.mp3')
-
-'''import pygame
-pygame.init()
-pygame.mixer.music.load('disagree.mp3')
-'''
+import pygame
+pygame.init
+pygame.mixer.load('disagree.mp3')
+pygame.mixer.mixer.play
+pygame.event.wait()
