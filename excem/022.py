9c8719740dbd3de70269cba0de56cf0db2c4c027
Megami.ay <megami.ay@gmail.com>
2023-01-31 20:07:18 -0300

    022

---
+++ excem/022.py
@@ -0,0 +1,7 @@
+name = input('Digite seu nome: ')
+nawo = name.split(' ')
+print(name.upper())
+print(name.lower())
+print(nawo)
+# print(len(nawo[])) - dúvida ao imprimir a quantidade total de caracteres, sem espaços
+print(len(nawo[0]))
