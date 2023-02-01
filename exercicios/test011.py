80ed45f5902eec88a446b184d35510641f463086
Megami.ay <megami.ay@gmail.com>
2023-02-01 19:30:40 -0300

    test ifelse 2

--- exercicios/test011.py
+++ exercicios/test011.py
@@ -1,4 +1,9 @@
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 me = (n1 + n2)/2
-print(me)
+print('Sua média é: {}'.format(me))
+if me >= 7:
+    print('Sua média está aceitável')
+else:
+    print('Sua média não é o suficiente')
+print('Continue estudando')
