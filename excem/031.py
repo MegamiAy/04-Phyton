3cb5f702d67817aa1deb016821918a9990ba2d64
Megami.ay <megami.ay@gmail.com>
2023-02-01 22:28:16 -0300

    31 finish

--- excem/031.py
+++ excem/031.py
@@ -2,7 +2,7 @@ km = float(input('Digite a distância da sua viagem em Km: '))
 du = km*0.45
 me = km*0.50
 if km > 200:
-    print('o valor da viagem de até 200km será: {}'.format(du))
+    print('o valor da viagem acima de 200km será: {}'.format(me))
 else:
-    print('o valor da viagem será de: {}'.format(me))
+    print('o valor da viagem será de: {}'.format(du))
 print('Boa viagem')
