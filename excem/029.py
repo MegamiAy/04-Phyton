5e6e67dbdf04e4db5c291eaae5d26844c0ebc921
Megami.ay <megami.ay@gmail.com>
2023-02-02 11:36:38 -0300

    29

--- excem/029.py
+++ excem/029.py
@@ -1,4 +1,4 @@
-vel = float(input('Digite a velocidade que você dirige: '))
+vel = float(input('Digite a velocidade atual do seu veiculo: '))
 mul = (vel - 80) * 7
 if vel > 80:
     print('Você está sendo multado, no valor de {} por ter ultrapassado a velocidade limite'.format(mul))
