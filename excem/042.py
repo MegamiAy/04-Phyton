156b7d7df4c7023b0d5a95b50515ca8e26fbd6e6
Megami.ay <megami.ay@gmail.com>
2023-02-04 14:29:38 -0300

    42

--- excem/042.py
+++ excem/042.py
@@ -2,12 +2,13 @@ print('Analisador de triângulos 2.0')
 a = float(input('Digite o primeiro segmento: '))
 b = float(input('Digite o segundo segmento: '))
 c = float(input('Digite o terceiro segmento: '))
-if a > b + c and b > a + c and c > a + b:
-    print('É possível formar um triângulo:')
-# faltou dizer c não forma um triângulo
-elif a == b == c:
-    print('Equilátero')
-elif a == b or b == c or a == c:
-    print('Isósceles')
+if a < b + c and b < a + c and c < a + b:
+    print('É possível formar um triângulo:', end='')
+    if a == b == c:
+        print(' Equilátero')
+    elif a != b != c != a:
+        print(' Escaleno')
+    else:
+        print(' Isóceles')
 else:
-    print('Escaleno')
+    print('Não é possível formar um triângulo')
