1d92165b99b0f8c03c15319ea3d68b32a48f2aca
Megami.ay <megami.ay@gmail.com>
2023-02-04 14:40:20 -0300

    dsf43

--- excem/043.py
+++ excem/043.py
@@ -1,15 +1,14 @@
-'''alt = float(input('Digite sua altura em metros: '))
-pes = float(input('Digite seu peso: '))
-imc = (alt * alt) / pes
-if imc > 40:
-    print('obesidade mórbida')
-elif imc < 18.5:
+alt = float(input('Digite sua altura em metros: '))
+pes = float(input('Digite seu peso em quilos: '))
+imc = pes / (alt ** 2)
+print('Seu IMC é de {:.2f}'.format(imc))
+if imc < 18.5:
     print('abaixo do peso')
-elif imc >= 30 and imc <= 40:
-    print('obesidade')
-elif imc >= 35 and imc <= 30:
-    print('sobrepeso')
-elif imc >= 18.5 and imc <= 25:
+elif 18.5 <= imc < 25:
     print('peso ideal')
+elif 25 <= imc < 30:
+    print('sobre peso')
+elif 30 <= imc < 40:
+    print('obesidade')
 else:
-    print('valores na forma incorreta')'''
+    print('obesidade mórbida')
