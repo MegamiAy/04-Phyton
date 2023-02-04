732094452874b5276b68207e4826fab950b2c741
Megami.ay <megami.ay@gmail.com>
2023-02-03 23:31:22 -0300

    43?

---
+++ excem/043.py
@@ -0,0 +1,15 @@
+'''alt = float(input('Digite sua altura em metros: '))
+pes = float(input('Digite seu peso: '))
+imc = (alt * alt) / pes
+if imc > 40:
+    print('obesidade mórbida')
+elif imc < 18.5:
+    print('abaixo do peso')
+elif imc >= 30 and imc <= 40:
+    print('obesidade')
+elif imc >= 35 and imc <= 30:
+    print('sobrepeso')
+elif imc >= 18.5 and imc <= 25:
+    print('peso ideal')
+else:
+    print('valores na forma incorreta')'''
