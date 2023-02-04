f78c41dc2314f7810bc1c31a5f945991e13a52ab
Megami.ay <megami.ay@gmail.com>
2023-02-04 14:05:40 -0300

    news

--- excem/040.py
+++ excem/040.py
@@ -3,7 +3,7 @@ m2 = float(input('Digite a sua segunda nota: '))
 me = (m1 + m2) / 2
 if me >= 7:
     print('Aprovado')
-elif me >= 5 and me <= 6.9:
+elif 7 > me >= 5: # ou me >= 5 and me < 7
     print('Recuperação')
 elif me < 5:
     print('Reprovado')
