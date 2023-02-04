4746d389bb675d65472e296cb6a33a794d11aee2
Megami.ay <megami.ay@gmail.com>
2023-02-03 22:05:19 -0300

    36

--- excem/036.py
+++ excem/036.py
@@ -1,4 +1,4 @@
 pri = float(input('Digite o valor da casa R$'))
 mon = float(input('Digite seu salário R$'))
 yea = float(input('Digite em quantos anos você irá pagar: '))
-neg = sal + (sal * 30/100)
+neg = mon * 30/100
