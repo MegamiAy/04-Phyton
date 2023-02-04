9a05c50963f3fd66ac63fa03e6d63e0bd6ba1de1
Megami.ay <megami.ay@gmail.com>
2023-02-04 14:16:41 -0300

    41

--- excem/041.py
+++ excem/041.py
@@ -1,4 +1,16 @@
-'''from datetime import date
+from datetime import date
 nasc = int(input('Digite o seu ano de nascimento: '))
-num = .date()
-mi = nasc - num'''
+atu = date.today().year
+age = nasc - atu
+print('Você tem {} anos'.format(age))
+if age <= 9:
+    print('Mirim')
+elif age <= 14:
+    print('Infantil')
+elif age <= 19:
+    print('Junior')
+elif age <= 25:
+    print('Sênior')
+else:
+    print('Master')
+print('Fim...')
