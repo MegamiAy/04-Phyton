2657be4876a7fb43a6224fca79fc9be25dc2d45a
Megami.ay <megami.ay@gmail.com>
2023-02-04 13:34:39 -0300

    36

--- excem/036.py
+++ excem/036.py
@@ -1,4 +1,11 @@
 pri = float(input('Digite o valor da casa R$'))
 mon = float(input('Digite seu salário R$'))
 yea = float(input('Digite em quantos anos você irá pagar: '))
-neg = mon * 30/100
+pre = pri / (yea * 12) # prestação
+neg = mon * 30 / 100
+print('Para pagar um casa de R${:.2f} em {} anos'.format(pri, yea), end='')
+print(' a prestação será de R${:2f}'.format(pre))
+if pre <= neg:
+    print('Empréstimo pode ser consedido')
+else:
+    print('Empréstimo negado')
