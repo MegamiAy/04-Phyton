82c44ad5be7a9021e2919430189570d335333a71
Megami.ay <megami.ay@gmail.com>
2023-02-11 12:55:25 -0300

    51

--- excem/050.py
+++ excem/050.py
@@ -1,6 +1,6 @@
 for pi in range(0, 6):
     n = int(input('Digite um número inteiro: '))
 if n % 2 == 0:
-    print(pi)
+    print()
 else:
     print('Null')
---
+++ excem/051.py
@@ -0,0 +1,5 @@
+te = float(input('Digite o valor do primeiro termo: '))
+ra = float(input('Digite o valor da razão: '))
+for pa in range(1, 10+1):
+    ter = te + ra
+    print('a{} = {}'.format(pa, te+ra, te+ra))
