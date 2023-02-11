74a88cc5741bf8f5d4492c83cabc943818318c62
Megami.ay <megami.ay@gmail.com>
2023-02-11 12:45:06 -0300

    50

--- excem/048.py
+++ excem/048.py
@@ -1,1 +1,3 @@
-for som in range(1, 500, 2):
+'''for som in range(1, 500):
+    if som / 3:
+        print(som)'''
---
+++ excem/050.py
@@ -0,0 +1,6 @@
+for pi in range(0, 6):
+    n = int(input('Digite um número inteiro: '))
+if n % 2 == 0:
+    print(pi)
+else:
+    print('Null')
