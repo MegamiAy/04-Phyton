180a21f4281616647a7f888dab4441471166531b
Megami.ay <megami.ay@gmail.com>
2023-02-11 13:33:38 -0300

    50

--- excem/050.py
+++ excem/050.py
@@ -1,6 +1,9 @@
+s = 0
+c = 0
 for pi in range(0, 6):
-    n = int(input('Digite um número inteiro: '))
-if n % 2 == 0:
-    print()
-else:
-    print('Null')
+    n = int(input('Digite o {}º número inteiro: '.format(pi)))
+    if n % 2 == 0:
+        s += n
+        c += 1
+print('Você informou {} números pares e a soma foi {}'.format(c, s))
+
