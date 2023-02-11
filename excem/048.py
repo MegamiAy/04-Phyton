8d116b73bf31d3887188292d9b3a8e6eff7ca672
Megami.ay <megami.ay@gmail.com>
2023-02-11 13:23:21 -0300

    48

--- excem/048.py
+++ excem/048.py
@@ -1,3 +1,7 @@
-'''for som in range(1, 500):
-    if som / 3:
-        print(som)'''
+soma = 0
+cont = 0
+for som in range(1, 501, 2):
+    if som % 3 == 0:
+        cont += 1 # cont = cont + 1
+        soma += som # soma = soma + som
+print('A soma dos {} valores solicitados é {}'.format(cont, soma))
