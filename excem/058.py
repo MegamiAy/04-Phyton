b10ab7075a185adee9db1caea8256cf1ffc049a1
Megami.ay <megami.ay@gmail.com>
2023-02-12 18:23:32 -0300

    58

--- excem/058.py
+++ excem/058.py
@@ -1,9 +1,27 @@
-from random import randint
+'''from random import randint
 pc = randint(0, 10)
 print('O computador pensou em um número, tente acertar')
 res = ''
 ten = ''
 while res != pc:
     res = int(input('Digite sua tentativa: '))
-    res += 1
-print('Foram necessárias {} tentativas para você acertar'.format(res))
+    ten += 1
+print('Foram necessárias {} tentativas para você acertar'.format(ten))'''
+
+from random import randint
+pc = randint(0, 10)
+print('O computador pensou em um número, tente acertar')
+ac = False
+te = 0
+while not ac:
+    jg = int(input('Digite seu palpite'))
+    te += 1
+    if jg == pc:
+        ac = True
+    else:
+        if jg < pc:
+            print('Mais...')
+        elif jg > pc:
+            print('Menos...')
+print('Acertou')
+print('Foram {} tentativas para acertar'.format(te))
