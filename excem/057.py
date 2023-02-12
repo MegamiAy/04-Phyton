11822894925fdb64ebaa13fbd46ae5a7ab1e8246
Megami.ay <megami.ay@gmail.com>
2023-02-12 13:33:13 -0300

    exs

--- excem/057.py
+++ excem/057.py
@@ -1,6 +1,6 @@
 S = ''
 while S != 'F' and 'M':
-    S = str(input('Digite seu sexo: [F/M]')).upper()
+    S = str(input('Digite seu sexo: [F/M]')).strip().upper()
     if S == 'F':
         print('Você é uma mulher')
     elif S == 'M':
