245c4620a43d9cb5a2686df218e20c1ace36a4b0
Megami.ay <megami.ay@gmail.com>
2023-02-12 18:42:34 -0300

    59

--- excem/059.py
+++ excem/059.py
@@ -10,14 +10,16 @@ print('''Menu
 while op != 5:
     op = int(input('Digite sua escolha: '))
     if op == 1:
-        print(n1 + n2)
+        print('{} + {} = {}'.format(n1, n2, n1 + n2))
     elif op == 2:
-        print(n1 * n2)
+        print('{} x {} = {}'.format(n1, n2, n1 * n2))
     elif op == 3:
         if n1 > n2:
-            print('O primeiro valor é o maior')
+            print('{} é maior que {}'.format(n1, n2))
+        elif n2 > n1:
+            print('{} é maior que {}'.format(n2, n1))
         else:
-            print('O segundo valor é o maior')
+            print('Não há números maiores, ambos são iguais')
     elif op == 4:
         n1 = float(input('Digite um valor: '))
         n2 = float(input('Digite outro valor: '))
