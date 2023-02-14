f5573c5e3c79cc14ea8ec9f280fca32874bb9a3c
Megami.ay <megami.ay@gmail.com>
2023-02-14 18:08:33 -0300

    60

--- excem/060.py
+++ excem/060.py
@@ -1,9 +1,14 @@
 '''from math import factorial
-n = float(input('Digite um valor para calcular seu fatorial'))
+n = float(input('Digite um valor para calcular seu fatorial '))
 f = factorial(n)
 # 5! = 5 . (n! - 1)'''
-n = int(input('Digite um valor para calcular seu fatorial'))
+n = int(input('Digite um valor para calcular seu fatorial: '))
 c = n
+f = 1
+print('Calculando {}! = '.format(n), end='')
 while c > 0:
-    print('{}'.format(c))
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
     c -= 1
+print('{}'.format(f))
