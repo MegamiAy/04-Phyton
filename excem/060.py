0ff7432735904f13109d92fb22fc385178a662f9
Megami.ay <megami.ay@gmail.com>
2023-02-14 18:00:12 -0300

    Update 060.py

--- excem/060.py
+++ excem/060.py
@@ -1,7 +1,7 @@
-'''n = float(input('Digite um valor '))
+n = float(input('Digite um valor '))
 f = n - 1
 # 5! = 5 . (n! - 1)
 while n == 1:
     n += 1
     fa = n * f
-    print('Resultado: {}'.format(fa))'''
+    print('Resultado: {}'.format(fa))
